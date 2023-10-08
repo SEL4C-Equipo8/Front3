cd04e94deb521c52b5ce677e0066be7699dab226
JoseRobertoGC <a01656294@tec.mx>
2023-10-07 22:07:16 -0600

    Modulos

--- prueba/src/Modulos.js
+++ prueba/src/Modulos.js
@@ -2,59 +2,29 @@ import React, { useState } from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Modulos.css';
-import CustomModal from './components/CustomModal'
-
+import CustomModal from './components/CustomModal';
 
 const YourComponent = () => {
   const boxesData = [
-    {title: 'Box 1',},
-    {
-      title: 'Box 2',
-    },
-    {
-      title: 'Box 3',
-    },
-    {
-      title: 'Box 4',
-    },
-    {
-      title: 'Box 5',
-      
-    },
-    {
-      title: 'Box 6',
-      
-    },
-    {
-      title: 'Box 7',
-      
-    },
-    {
-      title: 'Box 8',
-      
-    },
-    {
-      title: 'Box 9',
-      
-    },
-    {
-      title: 'Box 10',
-     
-    },
-    {
-      title: 'Box 11',
-      
-    },
-    {
-      title: 'Box 12',
-      
-    },
+    { title: 'Box 1' },
+    { title: 'Box 2' },
+    { title: 'Box 3' },
+    { title: 'Box 4' },
+    { title: 'Box 5' },
+    { title: 'Box 6' },
+    { title: 'Box 7' },
+    { title: 'Box 8' },
+    { title: 'Box 9' },
+    { title: 'Box 10' },
+    { title: 'Box 11' },
+    { title: 'Box 12' },
   ];
 
+  const [showModal, setShowModal] = useState(false);
+  const [activityToEdit, setActivityToEdit] = useState(null);
 
-  const [showModal, setShowModal] = useState(false); // Nuevo estado para controlar la visibilidad del modal
-
-  const handleOpenModal = () => {
+  const handleOpenModal = (boxTitle, activityIndex) => {
+    setActivityToEdit({ boxTitle, activityIndex });
     setShowModal(true);
   };
 
@@ -62,42 +32,68 @@ const YourComponent = () => {
     setShowModal(false);
   };
 
+  const chunkArray = (array, chunkSize) => {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+      chunks.push(array.slice(i, i + chunkSize));
+    }
+    return chunks;
+  };
+
+  const groupedBoxes = chunkArray(boxesData, 3);
+
   return (
     <Container>
       <h1 style={{ textAlign: 'center' }}>Módulos - Actividades</h1>
-      <Button variant="primary" onClick={handleOpenModal}>
-        Crear Módulo
-      </Button>
-      {boxesData.map((box, index) => (
-        <Col key={index} xs={12} sm={6} md={4}>
-          <div className="box-container">
-            <Card>
-              <Card.Body>
-                <Card.Title>{box.title}</Card.Title>
-                <Button
-                  variant="outline-secondary"
-                  onClick={() => handleOpenModal()}
-                >
-                  Modificar
+      {groupedBoxes.map((group, groupIndex) => {
+        const idActividad = 7;
+        return (
+          <div key={groupIndex}>
+            <Row className="d-flex justify-content-between align-items-center">
+              <Col xs={9}>
+                <h2>Actividad {idActividad}</h2>
+              </Col>
+              <Col xs={3} className="text-right">
+                <Button variant="primary" onClick={() => handleOpenModal(null, idActividad)}>
+                  Crear Módulo
                 </Button>
-                <Button
-                  variant="outline-secondary"
-                  onClick={() => {}}
-                >
-                  Eliminar
-                </Button>
-              </Card.Body>
-            </Card>
+              </Col>
+            </Row>
+            <Row>
+              {group.map((box, index) => (
+                <Col key={index} xs={12} sm={6} md={4}>
+                  <div className="box-container">
+                    <Card>
+                      <Card.Body>
+                        <Card.Title>{box.title}</Card.Title>
+                        <Button variant="outline-secondary" onClick={() => handleOpenModal(box.title, idActividad)}>
+                          Modificar
+                        </Button>
+                        <Button variant="outline-secondary" onClick={() => {}}>
+                          Eliminar
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+            <CustomModal
+              show={showModal && activityToEdit && activityToEdit.activityIndex === idActividad}
+              onHide={handleCloseModal}
+              idActividad={idActividad}
+            />
           </div>
-        </Col>
-      ))}
-      {/* Renderizar el modal */}
-      <CustomModal show={showModal} onHide={handleCloseModal} />
+        );
+      })}
     </Container>
   );
 };
 
 export default YourComponent;
+
+
+
 
 
 
--- prueba/src/components/CustomModal.js
+++ prueba/src/components/CustomModal.js
@@ -2,55 +2,49 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
+import { useState, useEffect } from 'react';
+
 
 function CustomModal(props) {
-  const { show, onHide, handleOpenModal } = props;
+  const { show, onHide, handleOpenModal, idActividad } = props;
+  const [apiContent, setApiContent] = useState(""); 
+
+  useEffect(() => {
+    if (show) {
+      // Construye la URL de la solicitud GET usando idActividad
+      const apiUrl = `http://localhost:8000/api/admin/activity/7/module/create/`;
+      console.log(apiUrl)
+      // Realiza la solicitud HTTP GET
+      fetch(apiUrl)
+        .then((response) => response.text())
+        .then((data) => {
+          setApiContent(data);
+        })
+        .catch((error) => {
+          console.error('Error al cargar la vista API:', error);
+        });
+    }
+  }, [show, idActividad]);
 
   return (
     <>
       <Modal size="lg" show={show} onHide={onHide}>
         <Modal.Header closeButton>
-          <Modal.Title>Identificación - Act 1</Modal.Title>
+          <Modal.Title>Identificación - Act {idActividad}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Título</Form.Label>
-              <Form.Control type="text" placeholder="Reflexión Inicial" autoFocus />
-            </Form.Group>
-
-            {/* Campo para imágenes */}
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
-              <Form.Label>Imágenes</Form.Label>
-              <Form.Control type="file" accept="image/*" multiple />
-            </Form.Group>
-
-            {/* Campo para archivos multimedia */}
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
-              <Form.Label>Archivos Multimedia</Form.Label>
-              <Form.Control type="file" accept="audio/*,video/*" multiple />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-              <Form.Label>Descripción</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-          </Form>
+          {/* Muestra el contenido de la vista API en el modal */}
+          <div dangerouslySetInnerHTML={{ __html: apiContent }} />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={onHide}>
             Close
           </Button>
-          <Button variant="primary" onClick={() => {
-            onHide();
-            handleOpenModal();
-          }}>
-            Save Changes
-          </Button>
         </Modal.Footer>
       </Modal>
     </>
   );
+
 }
 
 export default CustomModal;
